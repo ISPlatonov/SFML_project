0b2bfe6c01765a3730ec0c8ed13b0b83e3066b22
Илья Платонов <isplatonov@miem.hse.ru>
2022-05-21 17:01:31 +0300

    fullscreen with border

--- include/Constants.hpp
+++ include/Constants.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
+
 #define PIXEL_SIZE size_t(10)
-#define WINDOW_SIZE_X size_t(256)
-#define WINDOW_SIZE_Y size_t(128)
+#define WINDOW_SIZE_X size_t(sf::VideoMode::getDesktopMode().width / PIXEL_SIZE)
+#define WINDOW_SIZE_Y size_t(sf::VideoMode::getDesktopMode().height / PIXEL_SIZE)
 #define WINDOW_SIZE sf::Vector2u(WINDOW_SIZE_X, WINDOW_SIZE_Y)
 //#define WORK_SPACE sf::Rect(16, 8, 96, 48)
 #define FRAMERATE_LIMIT size_t(60)
