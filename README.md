75e2284015789b490a556979ca58a627f7137131
Илья Платонов <isplatonov@miem.hse.ru>
2022-06-03 05:00:55 +0300

    added timing control onto server

--- README.md
+++ README.md
@@ -8,15 +8,36 @@ In file `include/Constants.hpp` you can set pixel and window size up. Keep in mi
 
 ## Installation
 
-Create a directory for building and build the game inside it: 
+### Linux
 
-    mkdir build
-    cd build
-    cmake ..
-    make
+- install dependencies:
+
+```bash
+sudo apt-get update && sudo apt-get install libxrandr-dev libxcursor-dev libudev-dev libopenal-dev libflac-dev libvorbis-dev libgl1-mesa-dev libegl1-mesa-dev libdrm-dev libgbm-dev
+```
+
+- intall SFML (if it's not yet):
+
+```bash
+git clone https://github.com/SFML/SFML.git && cd SFML && git checkout 2.6.x && cd ..
+cmake -S SFML -B SFML/build -DCMAKE_INSTALL_PREFIX=SFML/install -DBUILD_SHARED_LIBS=FALSE
+cmake --build SFML/build --config Release --target install
+```
+
+- install the game:
+
+```bash
+cmake -B build -DCMAKE_INSTALL_PREFIX=install -DSFML_DIR=SFML/install/lib/cmake/SFML -DBUILD_SHARED_LIBS=FALSE
+cmake --build build --config Release --target install
+```
 
 ## Running
 
-After installation there appears `SFML_project` executable file and `textures` directory. Just run `SFML_project` file for starting the game :)
+`bin` directory will consist all binaries and textures:
 
-Now you can use `w`, `a`, `s` and `d` buttons for moving across the map.
+- `SFML_project` - game client executable
+
+    You can use `w`, `a`, `s` and `d` buttons for moving across the map.
+
+- `server` - game server executable
+- `textures` - dir with all textures
--- src/server.cpp
+++ src/server.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Network.hpp>
+#include <SFML/System.hpp>
 #include <map>
 #include <string>
 #include <cstdlib>
@@ -11,78 +12,67 @@
 #include <iostream>
 
 
+void UdpWorker(Multiplayer::UdpManager& UdpManager)
+{
+    for (size_t i = 0; i < UdpManager.getPlayerDataPool().size() + UDP_PACKETS_GAP; ++i)
+    {
+        UdpManager.receive();
+    }
+
+    for (auto iter = UdpManager.getPlayerDataPool().begin(); iter != UdpManager.getPlayerDataPool().end();)
+    {
+        std::cout << "unpacking player from pool: " << (*iter).first << std::endl;
+        auto x = (*iter).second.getPosition().x;
+        auto y = (*iter).second.getPosition().y;
+        auto ip = (*iter).second.getIp();
+        auto local_ip = (*iter).second.getLocalIp();
+        auto time = (*iter).second.getTime();
+        sf::Uint32 time_now = static_cast<sf::Uint32>(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count());
+        int ping = static_cast<int>(time_now) - static_cast<int>(time);
+        std::cout << "id: " << (*iter).first << ", last timepoint: " << std::to_string(time) << std::endl;
+
+        if (ping > MAX_PING)
+        {
+            std::cout << "reached MAX_PING" << std::endl;
+            UdpManager.removePlayerById((*iter++).first);
+            continue;
+        }
+        for (auto dest_iter = UdpManager.getPlayerDataPool().begin(); dest_iter != UdpManager.getPlayerDataPool().end();)
+        {
+            std::cout << "sending " << (*iter).first << " data to " << (*dest_iter).first << std::endl;
+            if ((*dest_iter).first == (*iter).first)
+            {
+                std::cout << "*dest_iter == *iter" << std::endl;
+                ++dest_iter;
+                std::cout << "made ++dest_iter" << std::endl;
+                continue;
+            }
+            sf::Packet data;
+            data << x << y << ip << local_ip << time;
+            UdpManager.send(data, sf::IpAddress((*dest_iter++).second.getLocalIp()));
+            std::cout << "sent" << std::endl;
+        }
+        ++iter;
+    }
+}
+
+
 int main()
 {
     std::cout << std::flush << std::endl;
-    /*sf::UdpSocket socket;
-    sf::UdpSocket socket_send;
-    unsigned short port = PORT_LISTEN;
-    unsigned short port_send = PORT_SEND;
-    auto broadcast_ip = sf::IpAddress::Any;
-    auto my_ip = sf::IpAddress::getPublicAddress();
-    auto my_local_ip = sf::IpAddress::getLocalAddress();
-    sf::IpAddress address_send(broadcast_ip);
-    socket.setBlocking(false);
-    sf::IpAddress address_receive(my_local_ip);
-    // bind the socket to a port
-    if (socket.bind(port) != sf::Socket::Done)
-        return EXIT_FAILURE;
-    if (socket_send.bind(port_send) != sf::Socket::Done)
-        return EXIT_FAILURE;*/
-
-    //sf::Packet data;
-    //sf::Vector2f multiplayer_position;
-    //std::map<std::string, Multiplayer::PlayerData> player_pool;
-    //std::set<std::string> ip_pool;
     Multiplayer::UdpManager UdpManager(sf::IpAddress::getLocalAddress(), sf::IpAddress::Any);
 
     while (true)
     {
         // needs another thread
-        //auto last_timepoint = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+        auto last_timepoint = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
         
-
-        for (size_t i = 0; i < UdpManager.getPlayerDataPool().size() + UDP_PACKETS_GAP; ++i)
-        {
-            UdpManager.receive();
-        }
-
-        for (auto iter = UdpManager.getPlayerDataPool().begin(); iter != UdpManager.getPlayerDataPool().end();)
-        {
-            std::cout << "unpacking player from pool: " << (*iter).first << std::endl;
-            auto x = (*iter).second.getPosition().x;
-            auto y = (*iter).second.getPosition().y;
-            auto ip = (*iter).second.getIp();
-            auto local_ip = (*iter).second.getLocalIp();
-            auto time = (*iter).second.getTime();
-            sf::Uint32 time_now = static_cast<sf::Uint32>(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count());
-            int ping = static_cast<int>(time_now) - static_cast<int>(time);
-            std::cout << "id: " << (*iter).first << ", last timepoint: " << std::to_string(time) << std::endl;
-
-            if (ping > MAX_PING)
-            {
-                std::cout << "reached MAX_PING" << std::endl;
-                UdpManager.removePlayerById((*iter++).first);
-                continue;
-            }
-            for (auto dest_iter = UdpManager.getPlayerDataPool().begin(); dest_iter != UdpManager.getPlayerDataPool().end();)
-            {
-                std::cout << "sending " << (*iter).first << " data to " << (*dest_iter).first << std::endl;
-                if ((*dest_iter).first == (*iter).first)
-                {
-                    std::cout << "*dest_iter == *iter" << std::endl;
-                    ++dest_iter;
-                    std::cout << "made ++dest_iter" << std::endl;
-                    continue;
-                }
-                sf::Packet data;
-                data << x << y << ip << local_ip << time;
-                UdpManager.send(data, sf::IpAddress((*dest_iter++).second.getLocalIp()));
-                std::cout << "sent" << std::endl;
-            }
-            ++iter;
-        }     
-        //while (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count() - last_timepoint < 1);
+        UdpWorker(UdpManager);
+ 
+        //while (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count() - last_timepoint < 10);
+        //std::cout << std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count() - last_timepoint) << std::endl;
+        auto dt = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count() - last_timepoint;
+        sf::sleep(((1000 / FRAMERATE_LIMIT) >= dt ? sf::milliseconds((1000 / FRAMERATE_LIMIT) - dt) : sf::Time()));
     }
     return EXIT_SUCCESS;
 }
